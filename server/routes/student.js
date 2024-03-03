3b4a8d2d049eca4148ff56ef311a43400557decd
Rayan soltan <rayansoltan99@gmail.com>
2024-03-04 00:11:45 +0200

    student registration

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { registerstudent, studentverification } from '../controllers/register.js';
-import { loginuser } from '../controllers/login.js';
+import { loginStudent } from '../controllers/login.js';
 import { validateToken } from '../Middleware/validateTokenHandler.js';
 import { requestPasswordReset, resetPassword } from '../controllers/forgetpass.js';
 import upload from '../controllers/imageuploadcontroller.js';
@@ -10,9 +10,9 @@ const router = express.Router();
 
 //router.use(validateToken); we can use this to apply the validatetoken on all routes, 
 //but here we don;t want to apply this validation on the registration
-router.post('/register', upload.single("imageID"),registerstudent)
+router.post('/register', upload.single("ID"),registerstudent)
 router.get('/registerverify', studentverification)
-router.post('/login', loginuser)
+router.post('/login', loginStudent)
 // router.get('/current', validateToken, currentuser)
 router.post('/resetpassverify',requestPasswordReset)
 router.post('/resetpass',resetPassword)
