105d28833991575f7dc2994ed254ceb872b0a9d2
Rayan soltan <rayansoltan99@gmail.com>
2024-03-10 23:16:41 +0200

    update certificate builder

--- server/controllers/tenantController.js
+++ server/controllers/tenantController.js
@@ -1,17 +1,5 @@
-//this is the organization admin:
-//analytics:(number of requests,number of documents/number of certificates) according to their status
-//filter analytics by date
-//customize certificate fields/customized document
-//view student requests
-//view student documnet
-//view uploaded certificates/transcripts
-//admin can verify certificate and create qrcode
-// issue certificate
-
 import connection from '../config/connection.js';
-import { uploadImage } from './imageuploadcontroller.js';
-
-
+import { uploadImageInFolder } from './imageuploadcontroller.js';
 
 
 const SaveVerifiedCertificate = async (req, res) => {
@@ -23,23 +11,24 @@ const SaveVerifiedCertificate = async (req, res) => {
 
         // Get the buffer containing image data
         const imageData = req.file.buffer;
-        const orgid='5';
-        const stdID='6';
-        // Upload the certificate image to Cloudinary
-        const uploadedCertificate = await uploadImage(imageData);
+        const orgId = '1';
+        const stdId = '2';
 
-        // Check if upload to Cloudinary was successful
-        if (!uploadedCertificate.secure_url) {
-            return res.status(500).json({ error: 'Failed to upload certificate to Cloudinary' });
+        // Upload the certificate image to Cloudinary
+        const uploadedCertificate = await uploadImageInFolder(imageData, "Verified-Certificates");
+        console.log('Uploaded Certificate:', uploadedCertificate);
+
+        if (!uploadedCertificate) {
+            throw new Error('Failed to upload certificate to Cloudinary');
         }
 
         // Save the secure URL and other certificate data to your database using a prepared statement
         const [results] = await connection.promise().execute(
-            'INSERT INTO verifiedcertificate (student_id, organization_id, verification_date, certificate_url) VALUES ( ?, ?, ?, ?)',
-            [stdID, orgid,'', uploadedCertificate.secure_url]
+            `INSERT INTO verifiedcertificate (student_id, organization_id, certificate_url) VALUES (?, ?, ?)`,
+            [stdId, orgId, uploadedCertificate]
         );
-        
-         
+        console.log('Database Insert Result:', results);
+
         // Return success response
         return res.status(200).json({ message: 'Certificate saved successfully', certificate: uploadedCertificate });
         
@@ -49,6 +38,4 @@ const SaveVerifiedCertificate = async (req, res) => {
     }
 };
 
-
 export default SaveVerifiedCertificate;
-
