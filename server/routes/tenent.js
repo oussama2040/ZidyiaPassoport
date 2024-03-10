8b2dbf5684fc78da8e311216aef0b64753957997
MarwaHaidar <131484013+MarwaHaidar@users.noreply.github.com>
2024-03-10 19:03:43 +0200

    certificate

--- server/routes/tenent.js
+++ server/routes/tenent.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import { loginTenent } from '../controllers/login.js';
 import { TenentrequestPasswordReset, TenentresetPassword } from '../controllers/forgetpass.js';
-
+import SaveVerifiedCertificate from '../controllers/tenantController.js';
 const router = express.Router();
+import upload from '../controllers/imageuploadcontroller.js';
 
 
 
 router.post('/login', loginTenent);
 router.post('/resetpassverify',TenentrequestPasswordReset)
 router.post('/resetpass',TenentresetPassword)
+router.post('/savecertificate',upload.single('certificateImage'),SaveVerifiedCertificate)
 
 
 export default router;
