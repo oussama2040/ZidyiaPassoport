df289eaeda417aae324f4831a96b5e1b370f4a22
oussama <ohamzeh10@gmail.com>
2024-03-17 15:23:57 +0200

    create Analytics

--- server/routes/certificateRoute.js
+++ server/routes/certificateRoute.js
@@ -3,7 +3,7 @@ import {
     createCertificate, updateCertificate,
     countTotalCertificates, countPendingCertificates, countApprovedCertificates, countRejectedCertificates
     , getAllCertificateRequests, updateCertificateRequestStatus,countCertificatesByStatusAndDateRange,updatefilledRequestStatus
-    ,getAllCertificateVerified
+    ,getAllCertificateVerified,countPendingDocuments,countApprovedDocuments,countRejectedDocuments,countTotalDocuments
 } from '../controllers/certificateController.js';
 
 const router = express.Router();
@@ -15,6 +15,11 @@ router.get('/certificates/organization/:organization_id/count/pending', countPen
 router.get('/certificates/organization/:organization_id/count/approved', countApprovedCertificates);
 router.get('/certificates/organization/:organization_id/count/rejected', countRejectedCertificates);
 router.get('/certificates/organization/count/byDate', countCertificatesByStatusAndDateRange);
+
+router.get('/documents/organization/:organization_id/count', countTotalDocuments);
+router.get('/documents/organization/:organization_id/count/pending', countPendingDocuments);
+router.get('/documents/organization/:organization_id/count/approved', countApprovedDocuments);
+router.get('/documents/organization/:organization_id/count/rejected', countRejectedDocuments);
 
 router.get('/certificates/:organizationId', getAllCertificateRequests);
 router.get('/certificatesverified/:organizationId', getAllCertificateVerified);
